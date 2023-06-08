d9f81ea20f925acdc051623b6ac13625055c0cf9
KmiloZ <3107320340407@ingenieria.usac.edu.gt>
2023-06-07 23:02:02 -0600

    Diseño Login
    Se complemento el diseño de inicio de sesión con las partes esenciales, pero aún es necesario desarrollar la funcionalidad.

--- proyecto MIA 1/loginInterfaz.py
+++ proyecto MIA 1/loginInterfaz.py
@@ -2,8 +2,22 @@ import tkinter as tk
 
 myWindow = tk.Tk()
 
-myWindow.title("Proyecto MIA 1")
-myWindow.geometry("1000x600")
+myWindow.title("Proyecto MIA 1 - LOGIN")
+myWindow.geometry("400x600")
 myWindow.resizable(False,False)
+myWindow.configure(background='#161c29')
+label = tk.Label(text="Nombre de Usuario",background='#161c29',fg='white')
+entry = tk.Entry()
+
+label2 = tk.Label(text="Contraseña",background='#161c29',fg='white')
+entry2 = tk.Entry()
+
+boton = tk.Button(text="Ingresar")
+
+label.pack()
+entry.pack()
+label2.pack()
+entry2.pack()
+boton.pack()
 
 myWindow.mainloop()
