e1eb491c49462751b9515315c20c264e1367a047
Douglas Rivera <dougdari@gmail.com>
2023-06-17 03:19:17 -0600

    correccion de la creacion del archivo

--- proyecto MIA 1/Nube.py
+++ proyecto MIA 1/Nube.py
@@ -19,6 +19,14 @@ def iniciosesion():
     credenciales = GoogleDrive(gauth)
     return credenciales
 
+def crear_archivo_texto(file_name, content,id_folder):
+    credenciales = iniciosesion()
+    arch = credenciales.CreateFile({'title': file_name,
+                                    'parents': [{"kind":"drive#fileLink",
+                                                 "id":id_folder}]})
+    arch.SetContentString(content)
+    arch.Upload()
+    pass
 
 def encontrar_directorio(credenciales, raiz, carpeta_nombre):
 
@@ -36,17 +44,35 @@ def agregar_carpeta(credenciales, raiz, carpeta_nombre):
 
 def recorrer_ruta_agregar_archivo(raiz, credenciales, ruta):
 
+    print(ruta)
+
+    if ruta[0] == '/':
+        ruta = ruta[ 1:len(ruta)]
+
+    if ruta[len(ruta)-1] == '/':
+        ruta = ruta[ 0:len(ruta) -1]
+
     carpetas_anidadas = ruta.split('/')    
+
+    print(carpetas_anidadas)
+
     auxRaiz = raiz
 
     for subCarpeta in carpetas_anidadas:
+        print(subCarpeta)
+
+        print(auxRaiz)
 
         exists, subcarpetaRaiz = encontrar_directorio(credenciales, auxRaiz, subCarpeta)
+
+        print(subcarpetaRaiz)
 
         if exists:
 
             auxRaiz = subcarpetaRaiz
         else:
+
+            print('llega')
 
             agregar_carpeta(credenciales, auxRaiz, subCarpeta)
             nada, subcarpetaRaiz = encontrar_directorio(credenciales, auxRaiz, subCarpeta)
@@ -54,29 +80,24 @@ def recorrer_ruta_agregar_archivo(raiz, credenciales, ruta):
 
     return auxRaiz
 
-def crear_archivo(ruta,nombre,contenido):
+def crear_archivo(id_folder,ruta,nombre,contenido):
 
     credenciales = iniciosesion()
+
+    #print(ruta)
+
     destinoId = recorrer_ruta_agregar_archivo(id_folder,credenciales,ruta)
+
+    print(destinoId)
     arch = credenciales.CreateFile({'title': nombre,
                                     'parents': [{"kind":"drive#fileLink",
-                                                 "id":destinoId}]})
+                                                "id":destinoId}]})
     arch.SetContentString(contenido)
-    arch.Upload()
-    pass
-
-
-def crear_archivo_texto(file_name, content,id_folder):
-    credenciales = iniciosesion()
-    arch = credenciales.CreateFile({'title': file_name,
-                                    'parents': [{"kind":"drive#fileLink",
-                                                 "id":id_folder}]})
-    arch.SetContentString(content)
     arch.Upload()
     pass
 
 if __name__ == '__main__':
     id_folder = '1woQAJWf0cI0Cv23WmjsCfmQiA0ulyHAD'
     crear_archivo_texto('Ejemplo1.txt','Contenido de archivo',id_folder)
-
-    crear_archivo('/Mi Carpeta/','archivoPrueba.txt','Este es el contenido del archivo \n otra linea')
+    
+    crear_archivo('/Mi Carpeta/Hola/','archivoPrueba.txt','Este es el contenido del archivo \n otra linea')
