8ccb51e500060457d46d73a01a157236ad00149a
KmiloZ <3107320340407@ingenieria.usac.edu.gt>
2023-06-17 23:27:48 -0600

    Configure Cloud
    El programa puede determinar cuando se quiere realizar operaciones en la nube

--- proyecto MIA 1/Nube.py
+++ proyecto MIA 1/Nube.py
@@ -1,11 +1,20 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 import regex as re
+import analizadorEntrada
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import os.path
+import os
+from datetime import date
+from datetime import datetime
+
 direc_credenciales = 'credentials_module.json'
 id_folder = '1ba5thhoBgCP04YAXIeIWeRZg9YMmFT_P'
 
 class NubeCm:
-    def iniciosesion(self,):
+    def iniciosesion(self):
         GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = direc_credenciales
         gauth = GoogleAuth()
         gauth.LoadCredentialsFile(direc_credenciales)
@@ -21,7 +30,40 @@ class NubeCm:
         credenciales = GoogleDrive(gauth)
         return credenciales
 
-    def crear_archivo_texto(self,file_name, content,id_folder):
+    def ejecutarComando(self,arreglo,area_output):
+        #------------------------------------
+        if(arreglo[0][0].lower() == "create"):
+            self.crear_archivo(id_folder,arreglo[0][1],arreglo[0][3],arreglo[0][2],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "delete"):
+            self.eliminar(arreglo[0][1],arreglo[0][2],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "copy"):
+            ruta_origen1 = self.limpiarRuta(arreglo[0][1])
+            ruta_destino1 = self.limpiarRuta(arreglo[0][2])
+            self.copiar(ruta_origen1,ruta_destino1,area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "transfer"):
+            ruta_origen = self.limpiarRuta(arreglo[0][1])
+            ruta_destino = self.limpiarRuta(arreglo[0][2])
+            self.transferir(ruta_origen,ruta_destino,arreglo[0][3],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "rename"):
+            self.cambiar_nombre_archivo(arreglo[0][1],arreglo[0][2],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "modify"):
+            self.cambiar_contenido_archivo(arreglo[0][1],arreglo[0][2],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "add"):
+            self.agregar_contenido_al_final(arreglo[0][1],arreglo[0][2],area_output)
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "backup"):
+            print("Respaldo")
+         #------------------------------------
+        elif(arreglo[0][0].lower() == "exec"):
+            self.ejecutarArchivo(arreglo[0][1],area_output)
+
+    def crear_archivo_texto(self,file_name,content,id_folder):
         credenciales = self.iniciosesion()
         arch = credenciales.CreateFile({'title': file_name,
                                         'parents': [{"kind":"drive#fileLink",
@@ -70,8 +112,7 @@ class NubeCm:
 
         return auxRaiz
 
-    def crear_archivo(self,id_folder,ruta,nombre,contenido):
-
+    def crear_archivo(self,id_folder,nombre,contenido,ruta,area_output):
         credenciales = self.iniciosesion()
         destinoId = self.recorrer_ruta_agregar_archivo(id_folder,credenciales,ruta)
         arch = credenciales.CreateFile({'title': nombre,
@@ -79,7 +120,10 @@ class NubeCm:
                                                     "id":destinoId}]})
         arch.SetContentString(contenido)
         arch.Upload()
-        pass
+        #Se crea el contenido para la bitacora
+        input_bt = "create -name:"+nombre+" -path:"+ruta+" -body:"+contenido
+        output_bt = "Archivo Creado Exitosamente"
+        self.generar_registro(input_bt,output_bt,area_output)
 
     def limpiarRuta(self,ruta):
         partes = ruta.split("/")
@@ -137,9 +181,9 @@ class NubeCm:
 
         return auxRaiz
     
-    def eliminar(self,ruta):
+    def eliminar(self,nombre,ruta,area_output):
         credenciales = self.iniciosesion()
-        ruta_limpia = self.limpiarRuta(ruta)
+        ruta_limpia = self.limpiarRuta(ruta+nombre)
         id = self.recorrer_ruta_agregar_archivo_retornar_id(id_folder,credenciales,ruta_limpia)
         if(id != None):
             archivo_eliminar = credenciales.CreateFile({'id':id})
@@ -183,7 +227,7 @@ class NubeCm:
         else:
             print("No existe la ruta")
     
-    def copiar(self,ruta_origen,ruta_destino):
+    def copiar(self,ruta_origen,ruta_destino,area_output):
         credenciales = self.iniciosesion()
         ruta_origen_limpia = self.limpiarRuta(ruta_origen)
         ruta_destino_limpia = self.limpiarRuta(ruta_destino)
@@ -203,7 +247,7 @@ class NubeCm:
                 ruta_destino=self.get_IDFolder(file_list,n)
                 self.crear_ruta_ct(id_origen,ruta_destino)
                   
-    def transferir(self,ruta_origen,ruta_destino):
+    def transferir(self,ruta_origen,ruta_destino,modo,area_output):
         credenciales = self.iniciosesion()
         ruta_origen_limpia = self.limpiarRuta(ruta_origen)
         ruta_destino_limpia = self.limpiarRuta(ruta_destino)
@@ -237,15 +281,46 @@ class NubeCm:
                 ruta_destino=self.get_IDFolder(lista2,arch)
                 self.crear_ruta_ct(arch['id'],ruta_destino)
 
+    def ejecutarArchivo(self,ruta,area_output):
+        archivo = open("."+ruta,"r")
+        #Se extrae la linea de configuración
+        config_parametros = archivo.readline().split(" ")
+        if(config_parametros[3].split("->")[1] == "true"):
+            llave = config_parametros[4].split("->")[1]
+            llave = llave.replace("\n","")
+            lista_comandos = self.encrip.desencriptar(str(archivo.read().replace("\n","")),llave).decode("utf-8","ignore")
+            for comando in lista_comandos.splitlines():
+                analizadorEntrada.comandos = []
+                self.ejecutarComando(analizadorEntrada.parser.parse(comando, lexer=analizadorEntrada.lexer),area_output)
+        else:
+            for linea in archivo:
+                analizadorEntrada.comandos = []
+                self.ejecutarComando(analizadorEntrada.parser.parse(linea, lexer=analizadorEntrada.lexer),area_output)
+
     def get_IDFolder(self,file_list,file_padre):
         for x in file_list:
             if(x['title'] == file_padre['title']):
                 ruta_destino = x['id']
         return ruta_destino
-        
 
-#crear_archivo_texto('Ejemplo1.txt','Contenido de archivo',id_folder)
-#crear_archivo(id_folder,'/Mi Carpeta/Hola/','archivoPrueba.txt','Este es el contenido del archivo \n otra linea')
-nb = NubeCm()
-nb.transferir('/Mi Carpeta/Hola/','/Carpeta 2/')
-#nb.crear_archivo(id_folder,'/Mi Carpeta/Hola/','archivoPrueba.txt','Este es el contenido del archivo \n otra linea')
+    def llenar_log(self,contenido):
+        ruta = "/logs/"+str(date.today())+"/"
+        #Se comprueba la existencia del directorio
+        credenciales = self.iniciosesion()
+        destinoId = self.recorrer_ruta_agregar_archivo(id_folder,credenciales,ruta)
+        arch = credenciales.CreateFile({'title': "log_archivos.txt",
+                                        'parents': [{"kind":"drive#fileLink",
+                                                    "id":destinoId}]})
+        arch.SetContentString(contenido)
+        arch.Upload()
+       
+    def generar_registro(self,entrada,salida,area_output):
+        tiempo = datetime.now()
+        tiempo_string = tiempo.strftime("%d/%m/%Y %H:%M:%S")
+        cnt_bitacora = tiempo_string + " - Input - "+entrada+"\n"
+        cnt_bitacora += tiempo_string + " - Output - "+salida+"\n"
+        area_output.insert(tk.END,cnt_bitacora+"\n")
+        self.llenar_log(cnt_bitacora)
+
+#nbcm = NubeCm()
+#nbcm.generar_registro("a","a")
